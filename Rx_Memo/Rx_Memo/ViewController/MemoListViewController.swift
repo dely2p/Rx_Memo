6970bf92fdcbd4a82d8b27b3610144ae9c771368
dely2p <lhyal2p@gmail.com>
2021-07-24 23:41:40 +0900

    AppDelegate 코드 추가

--- Rx_Memo/Rx_Memo/ViewController/MemoListViewController.swift
+++ Rx_Memo/Rx_Memo/ViewController/MemoListViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
-
+    @IBOutlet weak var listTableView: UITableView!
+    
     var viewModel: MemoListViewModel!
+    @IBOutlet weak var addButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +22,15 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.memoList
+            .bind(to: listTableView.rx.items(cellIdentifier: "Cell")) { row, memo, cell in
+                cell.textLabel?.text = memo.content
+                
+            }
+            .disposed(by: rx.disposeBag)
     }
 }
